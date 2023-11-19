b4009bf2fbc91b94257b7063201fb4e2059e97eb
FTXpart2 <122008092+FTXpart2@users.noreply.github.com>
2023-11-18 21:59:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import cv2 as cv
+import numpy as np
+camera= cv.Videocapture(0)
 
+while True:
+  _, frame = camera.read()
+  cv.imshow('Camera', frame)
+  laplacian = cv.Laplacian(frame, cv.CV_64F)
+  laplacian = np.uint8(laplacian)
+  cv.imshow("Laplacian", laplacian)
+
+  edges = cv.Canny(frame,100,100)
+  cv.imshow('Canny', edges)
+  
+  if cv.waitKey(S) == ord('x'):
+    break
+camera.release()
+cv.destroyAllWindows()
